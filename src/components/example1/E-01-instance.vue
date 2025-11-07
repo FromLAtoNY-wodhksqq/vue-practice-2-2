23408d3687334d370247db0a7bc2b384f9025bae
FromLAtoNY-wodhksqq <wodhksqq@gmail.com>
2025-11-08 07:25:33 +0900

    example 1수정

---
+++ src/components/example1/E-01-instance.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hello, {{ message }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = '최재완'
+const message = ref(name)
+</script>
+
+<style scoped>
+div {
+  color: blue;
+}
+</style>
---
+++ src/components/example1/E-02-reactive.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{ fullName }}</div>
+</template>
+
+<script>
+export default {
+  name: "E02Reactive",
+  data() {
+    return {
+      firstName: "Kyungsu",
+      lastName: "Lee"
+    };
+  },
+  mounted() {
+
+    console.log('mounted');
+
+    setTimeout(() => {
+      this.firstName = "KSL";
+    }, 2000);
+  },
+  computed: {
+    fullName() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+};
+</script>
