855b5fd9025fcb6abdcbf8304ea377a8b05fff27
FromLAtoNY-wodhksqq <wodhksqq@gmail.com>
2025-11-08 07:42:52 +0900

    ex4변경

--- src/components/example2/E-04-directives.vue
+++ src/components/example2/E-04-directives.vue
@@ -1,73 +1,61 @@
 <template>
   <div class="container">
 
+    <!-- v-for 예제 -->
     <ul>
       <li v-for="item in items" :key="item.id">
-        {{ item.name }}-{{item.id}}
+        {{ item.name }} - {{ item.id }}
       </li>
     </ul>
 
+    <!-- v-if / v-show / style 바인딩 예제 -->
     <div>
       <div v-if="isVisible">This is visible 1</div>
       <div :style="{ visibility: isVisible ? 'visible' : 'hidden' }">
         This is visible 2
       </div>
       <div v-show="isVisible">This is visible 3</div>
-      <button @click="isVisible = !isVisible">Toggle Visibility</button>
+      <button @click="toggleVisibility">Toggle Visibility</button>
     </div>
 
+    <!-- v-text / v-html / v-pre / 조건부 렌더링 -->
     <div>
       <div v-text="count"></div>
       <div v-html="`<strong>${count}</strong>`"></div>
       <div v-pre>{{ count }}</div>
 
-      <div id="div-1" v-if="count % 3 === 0"> count % 3 === 0</div>
-      <div id="div-2" v-else-if="count % 2 === 0"> count % 2 === 0</div>
-      <div id="div-4" v-else-if="count % 5 === 0"> count % 5 === 0</div>
+      <div id="div-1" v-if="count % 3 === 0">count % 3 === 0</div>
+      <div id="div-2" v-else-if="count % 2 === 0">count % 2 === 0</div>
+      <div id="div-4" v-else-if="count % 5 === 0">count % 5 === 0</div>
       <div id="div-3" v-else>Count is not divisible by 2 or 3</div>
 
-      <button @click="count++">Increment Count</button>
+      <button @click="incrementCount">Increment Count</button>
     </div>
 
   </div>
-
-<!--
- directives
- 1. v-if
- 2. v-else
- 3. v-else-if
- 4. v-show
- 5. v-for
- 6. v-on
- 7. v-bind
- 8. v-model
- 9. v-slot
-10. v-text
-11. v-html
-12. v-pre
-13. v-cloak
-14. v-once
-15. v-is
-16. v-memo
- -->
 </template>
 
-<script>
-export default {
-  name: "E04Directives",
-  data() {
-    return {
-      isVisible: true,
-      items: [
-        { id: 1, name: "Item 1" },
-        { id: 2, name: "Item 2" },
-        { id: 3, name: "Item 3" },
-        { id: 4, name: "Item 4" },
-      ],
-      count: 0,
-    };
-  }
-};
+<script setup>
+import { ref } from 'vue'
+
+// 반응형 변수 선언
+const isVisible = ref(true)
+const count = ref(0)
+const items = ref([
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+  { id: 3, name: "Item 3" },
+  { id: 4, name: "Item 4" },
+])
+
+// 함수 정의
+const toggleVisibility = () => {
+  isVisible.value = !isVisible.value
+}
+
+const incrementCount = () => {
+  count.value++
+}
 </script>
 
 <style scoped>
@@ -77,6 +65,5 @@ export default {
   margin: 0 auto;
   padding: 20px;
   grid-gap: 20px;
-
 }
 </style>
