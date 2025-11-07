87fe384beb92b9242db7d44041396a5798fdfee9
FromLAtoNY-wodhksqq <wodhksqq@gmail.com>
2025-11-08 07:37:53 +0900

    ex3수정

--- src/components/example1/E-03-binding.vue
+++ src/components/example1/E-03-binding.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
-    <input v-model="id" />
-    <input v-model="password" />
+    <input v-model="id" placeholder="ID 입력" />
+    <input v-model="password" placeholder="Password 입력" />
     <p>{{ message }}</p>
-    <button @click="() => {
-        this.message =this.id +' '+ this.password;
-    }">whoami</button>
+    <button @click="whoami">whoami</button>
   </div>
 </template>
 
-<script>
-export default {
-  name: "E03Binding",
-  data() {
-    return {
-      message: "Hello Vue",
-      id: '',
-      password: ''
-    };
-  }
-};
+<script setup>
+import { ref } from 'vue'
+
+// 반응형 상태 정의
+const id = ref('')
+const password = ref('')
+const message = ref('Hello Vue')
+
+// 이벤트 핸들러 함수 정의
+const whoami = () => {
+  message.value = `${id.value} ${password.value}`
+}
 </script>
+
+<style scoped>
+input {
+  display: block;
+  margin: 4px 0;
+}
+
+button {
+  margin-top: 6px;
+}
+</style>
